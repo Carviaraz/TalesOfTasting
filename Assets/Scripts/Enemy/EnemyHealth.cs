84076928b34a594873132f9bd0f410621631a626
Carviaraz <161716806+Carviaraz@users.noreply.github.com>
2025-03-24 19:42:32 +0700

    Boss
    Add
    - 2 Bosses
    - boss pattern
    - boss room
    - boss projectiles

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -1,50 +1,72 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour, ITakeDamage
 {
-    [SerializeField] private float health;
-
+    [SerializeField] private float maxHealth = 100f;
     private SpriteRenderer spriteRenderer;
-    private float enemyHealth;
+    private float currentHealth;
     private Color initialColor;
     private Coroutine colorCoroutine;
 
+    // Delegate for damage events
+    public delegate void DamageEventHandler(float currentHealth, float damage);
+    // Delegate for death events
     public delegate void DeathEventHandler(GameObject deadEnemy);
+
+    // Events that other components can subscribe to
+    public event DamageEventHandler OnDamage;
     public event DeathEventHandler OnDeath;
 
+    // Public getters for health information
+    public float CurrentHealth => currentHealth;
+    public float MaxHealth => maxHealth;
+    public float HealthPercentage => (maxHealth > 0) ? (currentHealth / maxHealth) : 0f;
 
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        // Initialize health in Awake to ensure it's set before other components access it
+        currentHealth = maxHealth;
     }
 
     void Start()
     {
-        enemyHealth = health;
+        // Make sure health is set in Start as well (redundant safety)
+        if (currentHealth <= 0)
+        {
+            currentHealth = maxHealth;
+        }
+
         initialColor = spriteRenderer.color;
+
+        // Debug log to verify health is initialized correctly
+        Debug.Log($"EnemyHealth initialized: {currentHealth}/{maxHealth} ({HealthPercentage:P2})");
     }
 
     public void TakeDamage(float damage)
     {
-        enemyHealth -= damage;
-        Debug.Log("enemyHealth = " +enemyHealth);
+        currentHealth -= damage;
+        Debug.Log($"Enemy health = {currentHealth}/{maxHealth} ({HealthPercentage:P2})");
+
+        // Trigger the damage event so other components know health has changed
+        OnDamage?.Invoke(currentHealth, damage);
+
         ShowDamageColor();
-        if (enemyHealth <= 0)
+
+        if (currentHealth <= 0)
         {
             OnDeath?.Invoke(gameObject);
             Destroy(gameObject);
         }
     }
 
-    private void ShowDamageColor() 
+    private void ShowDamageColor()
     {
         if (colorCoroutine != null)
         {
             StopCoroutine(colorCoroutine);
         }
-
         colorCoroutine = StartCoroutine(IETakeDamage());
     }
 
