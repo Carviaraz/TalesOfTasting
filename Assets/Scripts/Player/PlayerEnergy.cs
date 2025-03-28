2ec5fa5286b2eecdd4c813f43cb0110ee1cf8346
Carviaraz <161716806+Carviaraz@users.noreply.github.com>
2025-03-28 22:30:03 +0700

    Consume energy
    Consume energy every seconds

--- Assets/Scripts/Player/PlayerEnergy.cs
+++ Assets/Scripts/Player/PlayerEnergy.cs
@@ -1,20 +1,53 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerEnergy : MonoBehaviour
 {
     [Header("Player")]
     [SerializeField] private PlayerConfig playerConfig;
+    [SerializeField] private float energyDrainRate = 1f;
+    [SerializeField] private string dungeonSceneName = "Dungeon";
 
-    void Update()
+    private void OnEnable()
     {
-        //if (Input.GetKeyDown(KeyCode.F)) 
-        //{
-        //    UseEnergy(1f);
-        //}
-        //if (Input.GetKeyDown(KeyCode.G))
-        //{
-        //    RecoverEnergy(1f);
-        //}
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        StartEnergyDrain();
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+        StopEnergyDrain();
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == dungeonSceneName)
+        {
+            StartEnergyDrain();
+        }
+        else
+        {
+            StopEnergyDrain();
+        }
+    }
+
+    private void StartEnergyDrain()
+    {
+        if (SceneManager.GetActiveScene().name == dungeonSceneName)
+        {
+            InvokeRepeating(nameof(DrainEnergy), 1f, 1f); // Calls DrainEnergy every second
+        }
+    }
+
+    private void StopEnergyDrain()
+    {
+        CancelInvoke(nameof(DrainEnergy));
+    }
+
+    private void DrainEnergy()
+    {
+        UseEnergy(energyDrainRate);
     }
 
     public void UseEnergy(float amount)
@@ -26,10 +59,10 @@ public class PlayerEnergy : MonoBehaviour
         }
     }
 
-    public void RecoverEnergy(float amount) 
+    public void RecoverEnergy(float amount)
     {
         playerConfig.CurrentEnergy += amount;
-        if(playerConfig.CurrentEnergy > playerConfig.MaxEnergy)
+        if (playerConfig.CurrentEnergy > playerConfig.MaxEnergy)
         {
             playerConfig.CurrentEnergy = playerConfig.MaxEnergy;
         }
